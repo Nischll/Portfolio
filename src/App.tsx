bc9eeb8b352e54a559ee59a114a913b5e8df5166
Nischal Shrestha <nnischal9156@gmail.com>
2025-08-23 11:41:47 +0545

    initial push with tailwind configuration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <main>
+        <div className="text-gray-900">Direct utility</div>
+        <div className="text-amber-400">Custom class (apply)</div>
+        <button className="bg-success text-success-foreground px-4 py-2 rounded">
+          Branch
+        </button>
+      </main>
+    </>
+  );
+}
+
+export default App;
